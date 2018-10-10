8cb8e8c32ec9579750b90938a479abea552e71ff
Bryan Bacolod <bacolodbryan8@gmail.com>
2018-10-10 18:39:07 +0800

    Drawer Fragment

---
+++ app/src/main/java/com/bacolod/drawer/drawerfragment/TextFieldDemo.java
@@ -0,0 +1,19 @@
+package com.bacolod.drawer.drawerfragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TextFieldDemo extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View layout = inflater.inflate(R.layout.text_field_demo, container, false);
+        return layout;
+    }
+}
